51e1ae025f75a69f30b20c4d3166d699f4caf980
AlexHunterLeo <142971349+AlexHunterLeo@users.noreply.github.com>
2023-10-12 22:02:43 +0800

    Delete create_data_lists.py

--- create_data_lists.py
+++
@@ -1,6 +0,0 @@
-from utils import create_data_lists
-
-if __name__ == '__main__':
-    create_data_lists(voc07_path='D:\\paper\\VOCtrainval\\VOC2007',
-                      voc12_path='D:\\paper\\VOCtrainval\\VOC2012',
-                      output_folder='./')
